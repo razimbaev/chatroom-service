1c36127e34da7f1e8a984787a57cce23268aad4a
Rustam Azimbaev <rustam.azimbaev365@gmail.com>
2020-04-08 19:56:32 -0400

    Created project from spring initializr

---
+++ src/test/java/com/mychatroom/chatroom/ChatroomServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mychatroom.chatroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatroomServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
