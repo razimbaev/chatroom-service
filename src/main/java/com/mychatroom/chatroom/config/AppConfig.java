030d9de0f5238378ab7b10386f908149bccdc3e5
Rustam Azimbaev <rustam.azimbaev365@gmail.com>
2020-05-08 02:32:51 -0400

    Use DynamoDB instance in AWS (with TODO to resolve later)

--- src/main/java/com/mychatroom/chatroom/config/AppConfig.java
+++ src/main/java/com/mychatroom/chatroom/config/AppConfig.java
@@ -1,6 +1,6 @@
 package com.mychatroom.chatroom.config;
 
-import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
@@ -25,8 +25,12 @@ public class AppConfig {
 
     @Bean
     public DynamoDB dynamoDB() {
-        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().withEndpointConfiguration(
-                new AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "us-east-2"))
+        // TODO - find way to dynamically use localhost in dev and actual DynamoDB in prod
+//        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().withEndpointConfiguration(
+//                new AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "us-east-2"))
+//                .build();
+//        return new DynamoDB(client);
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().withRegion(Regions.US_EAST_2)
                 .build();
         return new DynamoDB(client);
     }
